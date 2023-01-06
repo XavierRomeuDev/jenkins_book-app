5c47e67edebd59e2620fac53c939246aab8f14b6
Xavier Dev <xaviermromeu@gmail.com>
2023-01-06 10:40:49 +0100

    prove github sync

--- src/main/java/daaw/bookapp/config/SecurityConfig.java
+++ src/main/java/daaw/bookapp/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+//prove
 @Configuration
 public class SecurityConfig {
 
